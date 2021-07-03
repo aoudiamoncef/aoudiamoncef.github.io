63ac8545eae905d9ebfaa3c069932498c00d20d3
AOUDIA <mf.aoudia@gmail.com>
2021-07-04 01:39:29 +0200

    refactor: complete update

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,7 @@
 const CACHE_VERSION = 1;
 
 const BASE_CACHE_FILES = [
-    '/css/asciidoctor.css',
-    '/css/aoudiamoncef.css',
-    '/css/monokai-sublime.min.css',
+    '/css/main.min.css',
     '/theme.css',
     '/theme.js',
     '/manifest.json',
